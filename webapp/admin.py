b5ed2ac7ece6838dc1c9403153b7e1e9fc678da2
JGONGSQ <james90jiajia@Hotmail.com>
2016-04-07 21:03:29 +1000

    Add admin and some minor changes

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from webapp.models import *
 
 # Register your models here.
+
+
+class IssueAdmin(admin.ModelAdmin):
+    list_display = ['user', 'title']
+    search_fields = ['user__username', 'title']
+admin.site.register(Issue, IssueAdmin)
+
