1af5c872f2762ac50359b0bd2f137c60056feb3f
JGONGSQ <james90jiajia@Hotmail.com>
2016-04-07 20:15:09 +1000

    Sample Auto testing added

--- webapp/tests.py
+++ webapp/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
-
+from django.contrib.auth.models import User
+from webapp.models import *
+from webapp.forms import *
+from django.utils import timezone
 # Create your tests here.
+
+
+class IssueFormTest(TestCase):
+
+    fixtures = [
+        'webapp_test_users.json'
+    ]
+
+    def setUp(self):
+        self.user = User.objects.get(id=1)
+        self.valid_data = {
+            'title': 'Testing Title',
+            'type': Issue.TYPE_BUG,
+            'summary': 'This is the testing summary',
+            'date_registered': timezone.now(),
+            'is_solved': False
+        }
+
+    def test_filled_form_is_valid(self):
+        form = IssueForm(self.user, self.valid_data)
+        self.assertTrue(form.is_valid(), "error are" + form.errors.as_text())
