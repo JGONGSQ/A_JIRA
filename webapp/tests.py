63535976839202527e8a0f33cb8c2915c2ddf716
JGONGSQ <james90jiajia@Hotmail.com>
2016-04-13 21:40:59 +1000

    JIRA api test with mock version 1.2

--- webapp/tests.py
+++ webapp/tests.py
@@ -1,5 +1,4 @@
 # Core Django imports
-from django.conf import settings
 from django.contrib.auth.models import User
 from django.test import TestCase
 from django.utils import timezone
@@ -8,18 +7,28 @@ from django.utils import timezone
 # Imports from apps
 from .models import Issue as Model_Issue
 from .forms import IssueForm
-from webapp.views import create_issue_jira
 
 # Imports from Python calls
 import unittest
-import mock
-import sys, os
-from jira import JIRA, Issue
-import json
+import sys, re, os, logging, getpass, random, string, traceback
+import inspect, pickle, platform
+import jira
+from jira import JIRA, Issue, JIRAError
+from time import sleep
+import pytest
+import py
+# Global variables
+TEST_ROOT = os.path.dirname(__file__)
+TEST_ICON_PATH = os.path.join(TEST_ROOT, 'icon.png')
+TEST_ATTACH_PATH = os.path.join(TEST_ROOT, 'tests.py')
+
+OAUTH = False
+CONSUMER_KEY = 'oauth-consumer'
+KEY_CERT_FILE = '/home/bspeakmon/src/atlassian-oauth-examples/rsa.pem'
+KEY_CERT_DATA = None
+
 
 # Create your tests here.
-
-
 class IssueFormTest(TestCase):
 
     # define some defaults data in the json file
@@ -44,67 +53,254 @@ class IssueFormTest(TestCase):
         self.assertTrue(form.is_valid(), "error are" + form.errors.as_text())
 
 
-# class JiraApiTest(TestCase):
-#
-#     # setup some defaults data for this test
-#     def setUp(self):
-#         self.issue_dict_valid = {
-#             'project': {'key': settings.TESTING_PROJECT},
-#             'summary': 'This the testing summary',
-#             'issuetype': {'name': 'Bug'}
-#         }
-#         self.issue_dict_invalid = {
-#             'title': 123,
-#             'type': '123',
-#             'summary': None,
-#             'date_registered': timezone.now(),
-#             'is_solved': False
-#         }
-#
-#     # test the JIRA api with valid data
-#     def test_jira_api_with_valid_data(self):
-#         created, msg = create_issue_jira(self.issue_dict_valid)
-#         self.assertTrue(created, "KeyError: {0}".format(str(msg)))
-#
-#     # test the JIRA api with invalid data
-#     def test_jira_api_with_invalid_data(self):
-#         created, msg = create_issue_jira(self.issue_dict_invalid)
-#         self.assertFalse(created, "KeyError: {0}".format(str(msg)))
+if 'CI_JIRA_URL' in os.environ:
+    not_on_custom_jira_instance = pytest.mark.skipif(True, reason="Not applicable for custom JIRA instance")
+    print('Picked up custom JIRA engine.')
+else:
+    def noop(arg):
+        return arg
+    not_on_custom_jira_instance = noop
 
 
-class MockHTTPResponse(object):
-    def __init__(self, status, body):
-        self.status = status
-        self.body = body
+class Singleton(type):
 
-    def read(self):
-        return self.body
+    def __init__(cls, name, bases, dict):
+        super(Singleton, cls).__init__(name, bases, dict)
+        cls.instance = None
+
+    def __call__(cls, *args, **kw):
+        if cls.instance is None:
+            cls.instance = super(Singleton, cls).__call__(*args, **kw)
+        return cls.instance
 
 
-class JIRAApiTest(unittest.TestCase):
+class JiraTestManager(object):
+    """
+    Used to instantiate and populate the JIRA instance with data used by the unit tests.
+    Attributes:
+        CI_JIRA_ADMIN (str): Admin user account name.
+        CI_JIRA_USER (str): Limited user account name.
+        max_retries (int): number of retries to perform for recoverable HTTP errors.
+    """
+    __shared_state = {}
+
+    def __init__(self):
+        self.__dict__ = self.__shared_state
+
+        if not self.__dict__:
+            self.initialized = 0
+
+            try:
+
+                if 'CI_JIRA_URL' in os.environ:
+                    self.CI_JIRA_URL = os.environ['CI_JIRA_URL']
+                    self.max_retries = 5
+                else:
+                    self.CI_JIRA_URL = "https://pycontribs.atlassian.net"
+                    self.max_retries = 5
+
+                if 'CI_JIRA_ADMIN' in os.environ:
+                    self.CI_JIRA_ADMIN = os.environ['CI_JIRA_ADMIN']
+                else:
+                    self.CI_JIRA_ADMIN = 'ci-admin'
+
+                if 'CI_JIRA_ADMIN_PASSWORD' in os.environ:
+                    self.CI_JIRA_ADMIN_PASSWORD = os.environ[
+                        'CI_JIRA_ADMIN_PASSWORD']
+                else:
+                    self.CI_JIRA_ADMIN_PASSWORD = 'sd4s3dgec5fhg4tfsds3434'
+
+                if 'CI_JIRA_USER' in os.environ:
+                    self.CI_JIRA_USER = os.environ['CI_JIRA_USER']
+                else:
+                    self.CI_JIRA_USER = 'ci-user'
+
+                if 'CI_JIRA_USER_PASSWORD' in os.environ:
+                    self.CI_JIRA_USER_PASSWORD = os.environ[
+                        'CI_JIRA_USER_PASSWORD']
+                else:
+                    self.CI_JIRA_USER_PASSWORD = 'sd4s3dgec5fhg4tfsds3434'
+
+                self.CI_JIRA_ISSUE = os.environ.get('CI_JIRA_ISSUE', 'Bug')
+
+                if OAUTH:
+                    self.jira_admin = JIRA(oauth={
+                        'access_token': 'hTxcwsbUQiFuFALf7KZHDaeAJIo3tLUK',
+                        'access_token_secret': 'aNCLQFP3ORNU6WY7HQISbqbhf0UudDAf',
+                        'consumer_key': CONSUMER_KEY,
+                        'key_cert': KEY_CERT_DATA})
+                else:
+                    if self.CI_JIRA_ADMIN:
+                        self.jira_admin = JIRA(self.CI_JIRA_URL, basic_auth=(self.CI_JIRA_ADMIN,
+                                                                             self.CI_JIRA_ADMIN_PASSWORD),
+                                               logging=False, validate=True, max_retries=self.max_retries)
+                    else:
+                        self.jira_admin = JIRA(self.CI_JIRA_URL, validate=True,
+                                               logging=False, max_retries=self.max_retries)
+                if self.jira_admin.current_user() != self.CI_JIRA_ADMIN:
+                    # self.jira_admin.
+                    self.initialized = 1
+                    sys.exit(3)
+
+                if OAUTH:
+                    self.jira_sysadmin = JIRA(oauth={
+                        'access_token': '4ul1ETSFo7ybbIxAxzyRal39cTrwEGFv',
+                        'access_token_secret':
+                            'K83jBZnjnuVRcfjBflrKyThJa0KSjSs2',
+                        'consumer_key': CONSUMER_KEY,
+                        'key_cert': KEY_CERT_DATA}, logging=False, max_retries=self.max_retries)
+                else:
+                    if self.CI_JIRA_ADMIN:
+                        self.jira_sysadmin = JIRA(self.CI_JIRA_URL,
+                                                  basic_auth=(self.CI_JIRA_ADMIN,
+                                                              self.CI_JIRA_ADMIN_PASSWORD),
+                                                  logging=False, validate=True, max_retries=self.max_retries)
+                    else:
+                        self.jira_sysadmin = JIRA(self.CI_JIRA_URL,
+                                                  logging=False, max_retries=self.max_retries)
+
+                if OAUTH:
+                    self.jira_normal = JIRA(oauth={
+                        'access_token': 'ZVDgYDyIQqJY8IFlQ446jZaURIz5ECiB',
+                        'access_token_secret':
+                            '5WbLBybPDg1lqqyFjyXSCsCtAWTwz1eD',
+                        'consumer_key': CONSUMER_KEY,
+                        'key_cert': KEY_CERT_DATA})
+                else:
+                    if self.CI_JIRA_ADMIN:
+                        self.jira_normal = JIRA(self.CI_JIRA_URL,
+                                                basic_auth=(self.CI_JIRA_USER,
+                                                            self.CI_JIRA_USER_PASSWORD),
+                                                validate=True, logging=False, max_retries=self.max_retries)
+                    else:
+                        self.jira_normal = JIRA(self.CI_JIRA_URL,
+                                                validate=True, logging=False, max_retries=self.max_retries)
+
+                # now we need some data to start with for the tests
+
+                # jira project key is max 10 chars, no letter.
+                # [0] always "Z"
+                # [1-6] username running the tests (hope we will not collide)
+                # [7-8] python version A=0, B=1,..
+                # [9] A,B -- we may need more than one project
+
+                prefix = 'Z' + (re.sub("[^A-Z]", "",
+                                       getpass.getuser().upper()))[0:6] + \
+                         chr(ord('A') + sys.version_info[0]) + \
+                         chr(ord('A') + sys.version_info[1])
+
+                self.project_a = prefix + 'A'  # old XSS
+                self.project_a_name = "Test user=%s python=%s.%s A" \
+                                      % (getpass.getuser(), sys.version_info[0],
+                                         sys.version_info[1])
+                self.project_b_name = "Test user=%s python=%s.%s B" \
+                                      % (getpass.getuser(), sys.version_info[0],
+                                         sys.version_info[1])
+                self.project_b = prefix + 'B'  # old BULK
+
+                try:
+                    self.jira_admin.project(self.project_a)
+                except Exception as e:
+                    logging.warning(e)
+                    pass
+                else:
+                    self.jira_admin.delete_project(self.project_a)
+
+                try:
+                    self.jira_admin.project(self.project_b)
+                except Exception as e:
+                    logging.warning(e)
+                    pass
+                else:
+                    self.jira_admin.delete_project(self.project_b)
+
+                self.jira_admin.create_project(self.project_a,
+                                               self.project_a_name)
+                self.project_a_id = self.jira_admin.project(self.project_a).id
+
+                self.jira_admin.create_project(self.project_b,
+                                               self.project_b_name)
+
+                self.project_b_issue1_obj = self.jira_admin.create_issue(project=self.project_b,
+                                                                         summary='issue 1 from %s'
+                                                                                 % self.project_b,
+                                                                         issuetype=self.CI_JIRA_ISSUE)
+                self.project_b_issue1 = self.project_b_issue1_obj.key
+
+                self.project_b_issue2_obj = self.jira_admin.create_issue(project=self.project_b,
+                                                                         summary='issue 2 from %s'
+                                                                                 % self.project_b,
+                                                                         issuetype={'name': self.CI_JIRA_ISSUE})
+                self.project_b_issue2 = self.project_b_issue2_obj.key
+
+                self.project_b_issue3_obj = self.jira_admin.create_issue(project=self.project_b,
+                                                                         summary='issue 3 from %s'
+                                                                                 % self.project_b,
+                                                                         issuetype={'name': self.CI_JIRA_ISSUE})
+                self.project_b_issue3 = self.project_b_issue3_obj.key
+
+            except Exception as e:
+                print(e, '12313131231231231231231231')
+                # exc_type, exc_value, exc_traceback = sys.exc_info()
+                formatted_lines = traceback.format_exc().splitlines()
+                msg = "Basic test setup failed: %s\n\t%s" % (
+                    e, "\n\t".join(formatted_lines))
+                logging.fatal(msg)
+                self.initialized = 1
+                pytest.exit("FATAL")
+
+            self.initialized = 1
+
+        else:
+            # already exist but we need to be sure it was initialized
+            counter = 0
+            while not self.initialized:
+                sleep(1)
+                counter += 1
+                if counter > 60:
+                    logging.fatal("Something is clearly not right with " +
+                                  "initialization, killing the tests to prevent a " +
+                                  "deadlock.")
+                    sys.exit(3)
 
 
-        # setup some defaults data for this test
+class IssueTests(unittest.TestCase):
+
     def setUp(self):
-        self.issue_dict_valid = {
-            'project': {'key': settings.TESTING_PROJECT},
-            'summary': 'This the testing summary',
-            'issuetype': {'name': 'Bug'}
-        }
+        self.test_manager = JiraTestManager()
+        self.jira = JiraTestManager().jira_admin
+        self.jira_normal = self.test_manager.jira_normal
+        self.project_b = self.test_manager.project_b
+        self.project_a = self.test_manager.project_a
+        self.issue_1 = self.test_manager.project_b_issue1
+        self.issue_2 = self.test_manager.project_b_issue2
+        self.issue_3 = self.test_manager.project_b_issue3
 
-    # def test_issue_search(self):
-    #     api_mock = mock.Mock()
-    #     Issue.api = mock.Mock(return_value=api_mock)
-    #     api_mock.get.return_value = MockHTTPResponse(200, '{"issue":[]}')
-    #     issue = Issue.search('jql')
-    #     api_mock.get.assert_called_with('search', {'jql': 'jql', 'fields': 'summary, status'})
-    #     issue = Issue.search('more jql', 'summary,description')
-    #     api_mock.get.assert_called_with('search', {'jql': 'more jql', 'fields': 'summary,description' })
+    def test_issue(self):
+        issue = self.jira.issue(self.issue_1)
+        self.assertEqual(issue.key, self.issue_1)
+        self.assertEqual(issue.fields.summary,
+                         'issue 1 from %s' % self.project_b)
 
-    def test_issue_create(self):
-        api_mock = mock.Mock()
-        JIRA().api = mock.Mock(return_value=api_mock)
-        print(JIRA().api)
-        api_mock.send.return_value = MockHTTPResponse(201, '')
-        issue = JIRA().create_issue(fields=self.issue_dict_valid)
-        api_mock.send.assert_called_with('POST', 'issue', {'fields': self.issue_dict_valid})
+    @unittest.skip("disabled as it seems to be ignored by jira, returning all")
+    def test_issue_field_limiting(self):
+        issue = self.jira.issue(self.issue_2, fields='summary,comment')
+        self.assertEqual(issue.fields.summary,
+                         'issue 2 from %s' % self.project_b)
+        comment1 = self.jira.add_comment(issue, 'First comment')
+        comment2 = self.jira.add_comment(issue, 'Second comment')
+        comment3 = self.jira.add_comment(issue, 'Third comment')
+        self.jira.issue(self.issue_2, fields='summary,comment')
+        logging.warning(issue.raw['fields'])
+        self.assertFalse(hasattr(issue.fields, 'reporter'))
+        self.assertFalse(hasattr(issue.fields, 'progress'))
+        comment1.delete()
+        comment2.delete()
+        comment3.delete()
+
+    def test_issue_equal(self):
+        issue1 = self.jira.issue(self.issue_1)
+        issue2 = self.jira.issue(self.issue_2)
+        issues = self.jira.search_issues('key=%s' % self.issue_1)
+        self.assertTrue(issue1 == issues[0])
+        self.assertFalse(issue2 == issues[0])
